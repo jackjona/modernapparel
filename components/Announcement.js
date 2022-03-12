c36ca0ee8eeddd58b43c326bec05d2137da3c1e6
Jack Jona <65679392+jackjona123@users.noreply.github.com>
2022-03-11 21:23:11 -0500

    add support Ukraine banner message

--- components/Announcement.js
+++ components/Announcement.js
@@ -1,10 +1,19 @@
+import Image from "next/image";
 import React from "react";
 
 const Announcement = () => {
   return (
+    <div className="flex justify-center bg-emerald-600 font-semibold text-white h-8 lg:h-10 leading-8 lg:leading-10 outline-none uppercase">
+      <div className="mt-1 lg:mt-1.5 mr-3" >
+        <Image src="/images/ukraine/ua.svg" alt="Picture of the author" width={38} height={24}/>
+      </div> 
+        We stand with the people of Ukraine.
+    </div>
+    /*
     <div className="flex justify-center bg-emerald-600 font-semibold text-white h-8 lg:h-10 leading-8 lg:leading-10 outline-none">
       FREE SHIPPING ON ORDERS $20+
     </div>
+    */
   );
 };
 
